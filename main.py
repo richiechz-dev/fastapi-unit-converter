a1966f30278d4c0676f42282441362ac8f81f3fc
Ricardo Chavez <gic.rc455@gmail.com>
2025-11-05 12:29:34 -0600

    Clarify docs for Fahrenheit to Celsius conversion
    Update endpoint docstring to correctly state Fahrenheit to Celsius
    conversion and include formula. Clarify Query parameter description and
    add explanatory comments for BaseModel/Field.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel, Field
 
 
 # Nos aseguramos que las salidad siempre sean de tipo float
+# Base Model declara modelos o esquemas que validad y serializan automaticamente datos
+# Field añade metadatos como description en este caso
 class ConvertionResult(BaseModel):
     value_fahrenheit: float = Field(..., description="Temperatura en grados fahrenheit")
     value_celsius: float = Field(..., description="Temperatura en grados celsius")
@@ -17,16 +19,19 @@ async def root():
     return {"Hola mundo"}
 
 
+# Usamos Query para declarar un parametro de consulta
 @app.get("/convertir/temperatura")
 async def convert_fahrenheit_to_celsius(
     fahrenheit: float = Query(
-        ..., description="Valor de temperatura Fahrenheit a convertir"
+        ..., description="Valor de temperatura en Fahrenheit para convertir en Celsius"
     ),
 ):
     """
-    Convierte un valor de temperatura de Celsius a Fahrenheit.
+    Convierte un valor de temperatura de Fahrenheit a Celsius
+    Fórmular: C = F (F - 32) * 5 / 9
     Fórmula: F = (C * 9/5) + 32
     """
+
     celsius = (fahrenheit - 32) * 5 / 9
 
     return ConvertionResult(value_celsius=celsius, value_fahrenheit=fahrenheit)
